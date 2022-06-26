5ac275e881d675c0d5a97942f3e9e24ffe6f322d
xNoix <sapdoscofre@gmail.com>
2022-06-26 13:06:23 -0400

    primera tarea

---
+++ src/com/company/DatosMain.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class DatosMain {
+    public static void main(String[] args) {
+
+        int numero1 = 1;
+        long numero2 = 123456789123456789L;
+        double numero3 = 1234.5678;
+        boolean result = false;
+        String saludo = "Hola mundo";
+
+
+        System.out.println(numero1);
+        System.out.println(numero2);
+        System.out.println(numero3);
+        System.out.println(result);
+        System.out.println(saludo);
+    }
+}
